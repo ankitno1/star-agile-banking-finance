88c906af72a1c222ad593f457774c3c52c1541bc
ankitno1 <jainankit.840@gmail.com>
2024-09-11 02:24:43 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,22 +1,22 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "testserver" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name = "keypair2"
-  vpc_security_group_ids = ["sg-0624c6a8b6420566d"]
+  key_name = "ankit"
+  vpc_security_group_ids = ["sg-0bc97f8af1eaa9600"]
   connection {
      type = "ssh"
      user = "ubuntu"
-     private_key = file("./keypair2.pem")
+     private_key = file("./ankit.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
      inline = ["echo 'wait to start the instance' "]
   }
   tags = {
-     Name = "test-server"
+     Name = "testserver"
      }
   provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     command = "echo ${aws_instance.testserver.public_ip} > inventory"
      }
   provisioner "local-exec" {
      command = "ansible-playbook /var/lib/jenkins/workspace/Care-Health/terraform-files/ansibleplaybook.yml"
